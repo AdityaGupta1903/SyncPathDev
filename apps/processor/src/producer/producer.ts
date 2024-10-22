053ea001e22adaa26818c5106d150e03e999c92e
AdityaGupta1903 <guptaditya19@gmail.com>
2024-10-22 08:53:49 +0530

    Broker Changed In Kafka

--- apps/processor/src/producer/producer.ts
+++ apps/processor/src/producer/producer.ts
@@ -2,7 +2,7 @@ import { Kafka } from "kafkajs"
 
 const kafka = new Kafka({
   clientId: 'my-app',
-  brokers: ['16.171.134.235:9092'],
+  brokers: ['localhost:9092', 'localhost:9092'],
 })
 
 export default kafka

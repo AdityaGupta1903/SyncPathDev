9421241d3fbd881f43255f4329b8635340bc8d21
Aditya Gupta <adityagu@vconstruct.in>
2024-12-29 14:31:27 +0530

    Changed Cors Url Configuration

--- apps/api-users/src/main.ts
+++ apps/api-users/src/main.ts
@@ -5,9 +5,9 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://syncpath.adityagupta.site/',
+    origin: 'https://localhost:3000',
     credentials: true,
-  })
+  });
   app.use(cookieParser());
 
   await app.listen(3002, () => {

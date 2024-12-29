541568e339e02923732b65b3c47c8d1d2e4b1a48
Aditya Gupta <adityagu@vconstruct.in>
2024-12-29 16:03:19 +0530

    Changed Cors policy to everyone

--- apps/api-users/src/main.ts
+++ apps/api-users/src/main.ts
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://syncpath.adityagupta.site/',
+    origin: '*',
     credentials: true,
   });
   app.use(cookieParser());
